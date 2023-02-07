318ced554ece5cb591e1cbb62f158dc5e54abfd4
Tassadite <k.d.h.mada@icloud.com>
2023-02-07 09:37:24 +0100

    appel fichier style css

--- wp-content/themes/blankslate-child/functions.php
+++ wp-content/themes/blankslate-child/functions.php
@@ -1,14 +1,9 @@
-<?php 
-add_action('wp_enqueue_scripts', 'blankslate_child_enqueue_styles');
+<?php
 
-function blankslate_child_enqueue_styles()
-{
-    wp_enqueue_style('blankslate-style', get_template_directory_uri() . '/style.css');
-    add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-
-function blanksalte_child_enqueue_styles()
-{
-    wp_enqueue_style('blankslate-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('blankslate_child-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
+add_action( 'wp_enqueue_scripts', 'blankslate_child_enqueue' );
+function blankslate_child_enqueue() {
+wp_enqueue_style( 'blankslate_child-style', get_stylesheet_uri() );
+wp_enqueue_script( 'jquery' );
 }
-}?>
+
+?>
