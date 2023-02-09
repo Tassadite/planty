2d94fd5f0b6e46c5d739e499e90b405b30501774
Tassadite <k.d.h.mada@icloud.com>
2023-02-09 10:05:45 +0100

    header footer

--- wp-content/themes/blankslate-child/functions.php
+++ wp-content/themes/blankslate-child/functions.php
@@ -1,14 +1,17 @@
 <?php
-
+/*appel du style*/
 add_action( 'wp_enqueue_scripts', 'blankslate_child_enqueue' );
+/*fonction d'activation du style du theme enfant*/
 function blankslate_child_enqueue() {
-wp_enqueue_style( 'blankslate_child-style', get_stylesheet_uri() . '/style.css');}
+    wp_enqueue_style( 'blankslate_child-style', get_stylesheet_directory_uri() . '/style.css');
+}
+/*fonction d'activation des menus*/
 function blankslate_child_supports()
 {
     register_nav_menu('header', 'En tête du menu');
     register_nav_menu('footer', 'Pied de page');
 }
-
+/*appel des menus*/
 add_action('after_setup_theme', 'blankslate_child_supports');
 
 ?>
