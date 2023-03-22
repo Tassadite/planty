084b8a4a5b8c0c7aa648002328fa5c3ce9b86111
Tassadite <k.d.h.mada@icloud.com>
2023-03-22 11:39:48 +0100

    page commander

--- wp-content/themes/blankslate-child/header.php
+++ wp-content/themes/blankslate-child/header.php
@@ -15,13 +15,12 @@ $custom_logo_id = get_theme_mod( 'custom_logo' );
 $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );?>
 
 <?php
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; }
 if ( has_custom_logo() ) {
   echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name"><img src="' . esc_url( $logo[0]) . '" alt="' . get_bloginfo( 'name' ) . '"></span></a>';
 } else {
   echo '<a href="' . esc_url( home_url( '/' ) ) . '" title="' . esc_attr( get_bloginfo( 'name' ) ) . '" rel="home" itemprop="url"><span itemprop="name">' . esc_html( get_bloginfo( 'name' ) ) . '</span></a>';
 }
-if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; }
+
 ?>
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
